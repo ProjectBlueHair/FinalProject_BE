d2ba0b193e2ae4ef33e1179b5d71c7286a85f406
JeekLee <j3kkun@gmail.com>
2022-12-30 15:40:07 +0900

    feat: Common Response 및 ErrorResponse 구현

---
+++ src/main/java/com/bluehair/hanghaefinalproject/common/exception/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.bluehair.hanghaefinalproject.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final Integer customHttpStatus;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .customHttpStatus(errorCode.getCustomHttpStatusCode())
+                        .message(errorCode.getMessage())
+                        .build());
+    }
+}
