8b63e23f9b4df10523831fb3af3ec25076e43a13
JeekLee <j3kkun@gmail.com>
2022-12-31 17:09:11 +0900

    feat: Mapstruct를 통한 Entity <-> Dto 변화을 위해 선언
    Mapstruct 적용

---
+++ src/main/java/com/bluehair/hanghaefinalproject/member/mapper/MemberMapStruct.java
@@ -0,0 +1,14 @@
+package com.bluehair.hanghaefinalproject.member.mapper;
+
+import com.bluehair.hanghaefinalproject.member.dto.SignUpMemberDto;
+import com.bluehair.hanghaefinalproject.member.dto.TestDto;
+import com.bluehair.hanghaefinalproject.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MemberMapStruct {
+    MemberMapStruct MEMBER_MAPPER = Mappers.getMapper(MemberMapStruct.class);
+    Member SignUpMemberDtoToMember(SignUpMemberDto signUpMemberDto);
+    TestDto MemberToTestDto(Member member);
+}
