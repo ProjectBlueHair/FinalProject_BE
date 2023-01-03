583d69ec5f58920fbf733330ad6534ebbfca902c
JeekLee <j3kkun@gmail.com>
2023-01-03 10:05:52 +0900

    feat: Jwt 관련 Exception 처리를 위해 구현
    Service 영역과 Filter에서 동시에 같은 Exception을 처리하기 위해 양쪽에 각각 구현하였습니다.

---
+++ src/main/java/com/bluehair/hanghaefinalproject/member/exception/TokenReissuanceExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.bluehair.hanghaefinalproject.member.exception;
+
+import com.bluehair.hanghaefinalproject.common.response.error.ErrorResponse;
+import com.bluehair.hanghaefinalproject.member.controller.MemberController;
+import com.bluehair.hanghaefinalproject.security.exception.CustomJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(assignableTypes = MemberController.class)
+public class TokenReissuanceExceptionHandler {
+    @ExceptionHandler(value = { CustomJwtException.class })
+    public ResponseEntity<ErrorResponse> handleTokenReissuance(CustomJwtException e) {
+        log.error("CustomJwtException throws : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}
