f9e0306acfc9af1cc415357dbcd7aee928be6c8a
JeekLee <j3kkun@gmail.com>
2023-01-03 10:02:47 +0900

    feat: Swagger 버전 업데이트 2.6 -> 3.0

--- src/main/java/com/bluehair/hanghaefinalproject/config/SwaggerConfig.java
+++ src/main/java/com/bluehair/hanghaefinalproject/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.bluehair.hanghaefinalproject.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,29 +8,23 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
-@EnableAutoConfiguration
 public class SwaggerConfig {
-    private final String version = "v1";
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("HangHae Final Project")
-                .description("Project Description")
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bluehair.hanghaefinalproject")) // api 스펙이 작성되어 있는 패키지 (controller)
+                .paths(PathSelectors.any())
                 .build();
     }
 
-    @Bean
-    public Docket commonApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName(version)
-                .apiInfo(this.apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bluehair.hanghaefinalproject"))
-                .paths(PathSelectors.any())
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("FinalProject Rest API Documentation")
+                .description("최종 프로젝트 API 명세")
+                .version("0.1")
                 .build();
     }
 }
