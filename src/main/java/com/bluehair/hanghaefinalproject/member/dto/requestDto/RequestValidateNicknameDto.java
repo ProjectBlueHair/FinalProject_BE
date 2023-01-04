33de342a102da54e99601e4208826634225b4a56
JeekLee <j3kkun@gmail.com>
2023-01-04 14:45:01 +0900

    fix: Swagger에서 DTO의 정보가 정상적으로 반영되지 않는 현상 수정

--- src/main/java/com/bluehair/hanghaefinalproject/member/dto/requestDto/RequestValidateNicknameDto.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/dto/requestDto/RequestValidateNicknameDto.java
@@ -2,8 +2,10 @@ package com.bluehair.hanghaefinalproject.member.dto.requestDto;
 
 import com.bluehair.hanghaefinalproject.member.dto.serviceDto.ValidateNicknameDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 @Schema(description = "닉네임 중복 확인 요청 Dto")
+@Getter
 public class RequestValidateNicknameDto {
     @Schema(description = "닉네임", required = true, example = "test01")
     private String nickname;
