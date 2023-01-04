e39fdeb8e4c1e3a4577b8864960eb54462f4c1ca
JeekLee <j3kkun@gmail.com>
2023-01-04 14:08:06 +0900

    docs: Swagger Document에서 Response 내역 반환 삭제
    Response에 대한 반환은 Response관련 클래스가 아닌 DTO로 변경하는 것이 좋을 것 같습니다.

--- src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SuccessResponse.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SuccessResponse.java
@@ -1,7 +1,6 @@
 package com.bluehair.hanghaefinalproject.common.response.success;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +9,8 @@ import org.springframework.http.ResponseEntity;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuccessResponse<T> {
-    @ApiModelProperty(notes = "커스텀 HTTP 상태 코드", example = "2000")
     private final Integer customHttpStatus;
-    @ApiModelProperty(notes = "반환 메세지", example = "회원 가입 성공")
     private final String message;
-    @ApiModelProperty(notes = "반환 데이터", example = "반환 데이터")
     private T data;
 
     public static<T> ResponseEntity<SuccessResponse<T>> toResponseEntity(SucessCode sucessCode, T data){
