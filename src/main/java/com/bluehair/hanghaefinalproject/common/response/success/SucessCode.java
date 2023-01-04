e76badf7f82305f6f078879746638d4278923695
yjk <djwhs415@gmail.com>
2023-01-04 14:15:51 +0900

    feat : 게시글 작성 기능 SucessCode 작성

--- src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SucessCode.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SucessCode.java
@@ -11,7 +11,9 @@ public enum SucessCode {
     // Member
     SIGNUP_MEMBER(HttpStatus.OK, "회원 가입 성공", 2000),
     LOGIN_MEMBER(HttpStatus.OK, "로그인 성공", 2000),
-    TOKEN_REISSUANCE(HttpStatus.OK, "토큰 재발행 성공", 2001);
+    TOKEN_REISSUANCE(HttpStatus.OK, "토큰 재발행 성공", 2001),
+
+    CREATE_POST(HttpStatus.OK, "게시글 작성 성공", 2000);
 
     private final HttpStatus httpStatus;
     private final String message;
