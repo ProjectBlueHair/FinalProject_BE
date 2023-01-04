3cfa839256310cdf64a3f453eea8246081025a26
JeekLee <j3kkun@gmail.com>
2023-01-04 14:02:39 +0900

    feat: 회원 가입 시, 이메일, 패스워드, 닉네임 관련 ErrorCode 생성
    중복 이메일, 중복 닉네임, 유효하지 않은 비밀번호

--- src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SucessCode.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SucessCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SucessCode {
     // Member
+    VALID_EMAIL(HttpStatus.OK, "사용 가능한 이메일", 2000),
+    VALID_NICKNAME(HttpStatus.OK, "사용 가능한 닉네임", 2000),
     SIGNUP_MEMBER(HttpStatus.OK, "회원 가입 성공", 2000),
     LOGIN_MEMBER(HttpStatus.OK, "로그인 성공", 2000),
     TOKEN_REISSUANCE(HttpStatus.OK, "토큰 재발행 성공", 2001);
