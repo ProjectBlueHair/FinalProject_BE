3e3c55f9255325ffa271c90644fc6654fd4bdd83
JeekLee <j3kkun@gmail.com>
2023-01-04 14:12:20 +0900

    feat: nickname을 통해 Member를 확인하는 메소드 추가

--- src/main/java/com/bluehair/hanghaefinalproject/member/repository/MemberRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/repository/MemberRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
 
 }
