1892eb67114c5bd57d7e5eeee4099aa869f0bd44
JeekLee <j3kkun@gmail.com>
2023-01-04 15:46:14 +0900

    fix: Signup Exception 의존 관계 변경(Runtime -> IllegalArguments)

--- src/main/java/com/bluehair/hanghaefinalproject/member/exception/InvalidSignUpRequestException.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/exception/InvalidSignUpRequestException.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class InvalidSignUpRequestException extends RuntimeException{
+public class InvalidSignUpRequestException extends IllegalArgumentException {
     private final ErrorCode errorCode;
 }
