25dd8e44b9d3fa269a9b179b0dc41412ffeea9c0
yjk <djwhs415@gmail.com>
2023-01-05 20:40:16 +0900

    feat : PostId로 collaboList를 불러오기 위한 쿼리 작성

--- src/main/java/com/bluehair/hanghaefinalproject/collaboRequest/repository/CollaboRequestRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/collaboRequest/repository/CollaboRequestRepository.java
@@ -3,6 +3,9 @@ package com.bluehair.hanghaefinalproject.collaboRequest.repository;
 import com.bluehair.hanghaefinalproject.collaboRequest.entity.CollaboRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CollaboRequestRepository extends JpaRepository<CollaboRequest, Long> {
 
+    List<CollaboRequest> findAllByPostId(Long id);
 }
