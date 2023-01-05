dd56e229d58a2a0df64d28151303cd5735be6a86
yjk <djwhs415@gmail.com>
2023-01-05 20:39:50 +0900

    feat : collaboId로 Music을 불러오기 위한 쿼리 작성

--- src/main/java/com/bluehair/hanghaefinalproject/music/repository/MusicRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/music/repository/MusicRepository.java
@@ -4,4 +4,5 @@ import com.bluehair.hanghaefinalproject.music.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MusicRepository extends JpaRepository<Music, Long> {
+    Music findByCollaboRequest_Id(Long id);
 }
