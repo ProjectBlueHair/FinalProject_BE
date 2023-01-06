972536a9f82fe84df851240d87d03132b548b718
JeekLee <j3kkun@gmail.com>
2023-01-06 10:38:19 +0900

    feat: AuthenticationPrinciple로 주어지는 Parameter값 제외

--- src/main/java/com/bluehair/hanghaefinalproject/config/SwaggerConfig.java
+++ src/main/java/com/bluehair/hanghaefinalproject/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.bluehair.hanghaefinalproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,6 +14,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
                 .select()
