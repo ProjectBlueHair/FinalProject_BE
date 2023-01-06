d937344efe2ae839b7d921ee7c56b055c1ab349a
JeekLee <j3kkun@gmail.com>
2023-01-06 10:37:43 +0900

    feat: SuccessResponse @Schema  설정

--- src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SuccessResponse.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/response/success/SuccessResponse.java
@@ -1,16 +1,21 @@
 package com.bluehair.hanghaefinalproject.common.response.success;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 
+@Schema(description = "요청 성공 시 ResponseDto")
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuccessResponse<T> {
+    @Schema(description = "Custom Http 상태 코드", example = "2000")
     private final Integer customHttpStatus;
+    @Schema(description = "상태 메세지", example = "~기능 성공")
     private final String message;
+    @Schema(description = "반환 데이터")
     private T data;
 
     public static<T> ResponseEntity<SuccessResponse<T>> toResponseEntity(SucessCode sucessCode, T data){
