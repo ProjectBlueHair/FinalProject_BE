10203b656387cf1ad3aa12695b9447ee05960ddb
yjk <djwhs415@gmail.com>
2023-01-07 18:58:46 +0900

    feat : Comment 도메인 Builder 패턴 적용

--- src/main/java/com/bluehair/hanghaefinalproject/comment/entity/Comment.java
+++ src/main/java/com/bluehair/hanghaefinalproject/comment/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.bluehair.hanghaefinalproject.comment.entity;
 
 import com.bluehair.hanghaefinalproject.post.entity.Post;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +30,13 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "POST_ID")
     private Post post;
+
+    @Builder
+    public Comment(Long parentsId, String nickname, String profileImg, String contents, Post post){
+        this.parentsId = parentsId;
+        this.nickname = nickname;
+        this.profileImg = profileImg;
+        this.contents = contents;
+        this.post = post;
+    }
 }
