fa9dd2f410fd3dfb674d8c18b311aea455adc897
JeekLee <j3kkun@gmail.com>
2023-01-07 20:22:54 +0900

    feat: Tag 관련 Repository 구현
    현재 saveAll 메소드 통해 저장되는 것으로 구현하였습니다.(@Query에서는 insert 미지원)
    saveAll이 save보다 약 60% 빠르다고 합니다

---
+++ src/main/java/com/bluehair/hanghaefinalproject/tag/repository/TagRepository.java
@@ -0,0 +1,12 @@
+package com.bluehair.hanghaefinalproject.tag.repository;
+
+import com.bluehair.hanghaefinalproject.tag.entity.Tag;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface TagRepository {
+    Tag save(Tag tag);
+    void saveTagList(List<Tag> tagList);
+}
