5444a1fbae7ecb80f60eba80e3439d92f2574950
sooni2 <jinong22@gmail.com>
2023-01-09 13:24:02 +0900

    feat - delete 쿼리에 @Transactional 추가

--- src/main/java/com/bluehair/hanghaefinalproject/music/repository/MusicRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/music/repository/MusicRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public interface MusicRepository extends JpaRepository<Music, Long> {
     List<Music> findAllByCollaboRequest(CollaboRequest collaboRequest);
 
     List<Music> findAllByCollaboRequestId(long collaboid);
-
+    @Transactional
     @Modifying
     @Query("DELETE from Music c where c.collaboRequest = :collaboRequest")
     void deleteAllByCollaboRequest(@Param("collaboRequest") CollaboRequest collaboRequestId);
