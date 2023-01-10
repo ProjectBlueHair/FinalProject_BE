aa1ac4e9504bdc9b1e2de7785460cadd3f74bc0c
JeekLee <j3kkun@gmail.com>
2023-01-10 11:30:29 +0900

    fix: IlleagalFormatException Custom 실패에 따른 IlleaglArgumentException 사용

--- src/main/java/com/bluehair/hanghaefinalproject/common/exception/FormatException.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/exception/FormatException.java
@@ -4,11 +4,10 @@ import com.bluehair.hanghaefinalproject.common.response.error.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.IllegalFormatException;
 
 @Getter
 @AllArgsConstructor
-public class FormatException extends IllegalFormatException {
+public class FormatException extends IllegalArgumentException {
     private Domain domain;
     private Layer layer;
     private ErrorCode errorCode;
