cd02f1be89877766c677b956333772223b9de327
JeekLee <j3kkun@gmail.com>
2023-01-10 11:42:18 +0900

    fix: 동일한 상태코드일 때, Swagger에 한 가지 Response만 반영되는 현상 수정
    동일한 상태코드일 경우, 2000-1, 2000-2와 같은 형태로 넘버링

--- src/main/java/com/bluehair/hanghaefinalproject/member/controller/MemberController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/controller/MemberController.java
@@ -8,8 +8,6 @@ import com.bluehair.hanghaefinalproject.member.service.MemberService;
 import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
