4ef16d2cd050e11732f7da7aa9dce0934bf5beef
JeekLee <j3kkun@gmail.com>
2023-01-12 09:53:08 +0900

    fix: MusicDto, SaveMusicDto 삭제 및 삭제에 따른 타 객체의 import 제거

--- src/main/java/com/bluehair/hanghaefinalproject/music/mapper/MusicMapStruct.java
+++ src/main/java/com/bluehair/hanghaefinalproject/music/mapper/MusicMapStruct.java
@@ -1,7 +1,5 @@
 package com.bluehair.hanghaefinalproject.music.mapper;
 
-import com.bluehair.hanghaefinalproject.collaboRequest.entity.CollaboRequest;
-import com.bluehair.hanghaefinalproject.music.dto.MusicDto;
 import com.bluehair.hanghaefinalproject.music.dto.ResponseMusicDto;
 import com.bluehair.hanghaefinalproject.music.entity.Music;
 
@@ -13,6 +11,5 @@ import java.util.List;
 @Mapper
 public interface MusicMapStruct {
     MusicMapStruct MUSIC_MAPPER = Mappers.getMapper(MusicMapStruct.class);
-    Music MusicDtotoMusic(MusicDto musicDto, CollaboRequest collaboRequest);
     List<ResponseMusicDto> MusictoResponseMusicDto (List<Music> musicList);
 }
