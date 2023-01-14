8a6121e5f3158f04cf2907f6e0520a3f6943cbdc
sooni2 <jinong22@gmail.com>
2023-01-14 15:02:18 +0900

    feat: swagger 컨벤션 추가

--- src/main/java/com/bluehair/hanghaefinalproject/postLike/controller/PostLikeController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/postLike/controller/PostLikeController.java
@@ -5,6 +5,8 @@ import com.bluehair.hanghaefinalproject.postLike.dto.ResponsePostLikeDto;
 import com.bluehair.hanghaefinalproject.postLike.service.PostLikeService;
 import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,11 @@ import static com.bluehair.hanghaefinalproject.common.response.success.SucessCod
 public class PostLikeController {
 
     private final PostLikeService postLikeService;
-
+    @Tag(name ="PostLike")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "2000", description = "좋아요/좋아요 취소 성공"),
+            @ApiResponse(responseCode = "4041", description = "존재하지 않는 게시글입니다.")
+    })
     @Operation(summary = "게시글 좋아요", description = "게시글 좋아요 표시")
     @PostMapping("/api/post/like/{postid}")
     public ResponseEntity<SuccessResponse<ResponsePostLikeDto>> postLike(@PathVariable Long postid, @AuthenticationPrincipal CustomUserDetails userDetails){
