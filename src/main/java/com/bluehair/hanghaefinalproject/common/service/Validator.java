4f0c769bb7f9bc70eece51422492586a403e5153
JeekLee <j3kkun@gmail.com>
2023-01-17 17:03:57 +0900

    feat: 음원 파일 유효성 확인 로직 구현

--- src/main/java/com/bluehair/hanghaefinalproject/common/service/Validator.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/service/Validator.java
@@ -1,5 +1,6 @@
 package com.bluehair.hanghaefinalproject.common.service;
 
+import javax.sound.sampled.AudioFormat;
 import java.util.regex.Pattern;
 
 public class Validator {
@@ -15,5 +16,12 @@ public class Validator {
         final String REGEX = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{" + MIN + "," + MAX + "}$";
 
         return Pattern.matches(REGEX, password);
+    }
+
+    public static boolean isValidAudioFormat(AudioFormat format){
+        if (format.getChannels() != 2 ) {
+            return false;
+        }
+        return format.getSampleSizeInBits() == 16 || format.getSampleSizeInBits() == 8;
     }
 }
