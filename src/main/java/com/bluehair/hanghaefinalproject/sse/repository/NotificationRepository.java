3a1b2800f19ee7e8cef220c4c8bd2d2dec6acff0
sooni2 <jinong22@gmail.com>
2023-01-26 19:19:43 +0900

    feat: 읽지않은알림 갯수 세는 Query 추가

--- src/main/java/com/bluehair/hanghaefinalproject/sse/repository/NotificationRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/sse/repository/NotificationRepository.java
@@ -3,9 +3,15 @@ package com.bluehair.hanghaefinalproject.sse.repository;
 import com.bluehair.hanghaefinalproject.member.entity.Member;
 import com.bluehair.hanghaefinalproject.sse.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findAllByReceiverOrderByCreatedAtDesc(Member member);
+
+    @Transactional
+    @Query("SELECT COUNT(n) from Notification n where n.receiver.nickname =:nickname and n.isRead = false")
+    Long countUnreadNotifications(String nickname);
 }
