1c8d2472c4c6119d7cfc1d0b9bdafbe33850a69f
yjk <djwhs415@gmail.com>
2023-01-27 19:05:07 +0900

    feat : ROOM 도메인 추가

--- src/main/java/com/bluehair/hanghaefinalproject/common/exception/Domain.java
+++ src/main/java/com/bluehair/hanghaefinalproject/common/exception/Domain.java
@@ -1,5 +1,5 @@
 package com.bluehair.hanghaefinalproject.common.exception;
 
 public enum Domain {
-    SECURITY, MEMBER, POST, COLLABO_REQUEST, MUSIC, COMMENT, TAG, LIKE, SSE
+    SECURITY, MEMBER, POST, COLLABO_REQUEST, MUSIC, COMMENT, TAG, LIKE, SSE, ROOM
 }
