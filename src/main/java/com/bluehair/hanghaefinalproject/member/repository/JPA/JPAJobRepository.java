7296b34b393b2c062ede2ba6cf5b20f432503589
JeekLee <j3kkun@gmail.com>
2023-01-27 13:04:14 +0900

    feat: JobRepository 전체 삭제 및 List 저장 기능 구현

--- src/main/java/com/bluehair/hanghaefinalproject/member/repository/JPA/JPAJobRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/repository/JPA/JPAJobRepository.java
@@ -1,8 +1,22 @@
 package com.bluehair.hanghaefinalproject.member.repository.JPA;
 
 import com.bluehair.hanghaefinalproject.member.entity.Job;
+import com.bluehair.hanghaefinalproject.member.entity.MemberDetail;
 import com.bluehair.hanghaefinalproject.member.repository.JobRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface JPAJobRepository extends JpaRepository<Job, Long>, JobRepository {
+    @Transactional
+    @Modifying
+    @Query("DELETE from Job t where t.memberDetail = :MemberDetail")
+    void deleteAllByMemberDetail(@Param("MemberDetail") MemberDetail memberDetail);
+    default void saveJobList(List<Job> jobList) {
+        saveAll(jobList);
+    }
 }
--- src/main/java/com/bluehair/hanghaefinalproject/member/repository/JobRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/repository/JobRepository.java
@@ -1,7 +1,12 @@
 package com.bluehair.hanghaefinalproject.member.repository;
 
 import com.bluehair.hanghaefinalproject.member.entity.Job;
+import com.bluehair.hanghaefinalproject.member.entity.MemberDetail;
+
+import java.util.List;
 
 public interface JobRepository {
     Job save(Job job);
+    void deleteAllByMemberDetail(MemberDetail memberDetail);
+    void saveJobList(List<Job> jobList);
 }
