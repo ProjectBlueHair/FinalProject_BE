593ef26b4ab1ec7ad9b367fb17021e82bc251802
sooni2 <jinong22@gmail.com>
2023-01-30 14:56:21 +0900

    fix: produce하는 context-type의 라이브러리 변경
    기존 swagger에서 java.http로 변경

--- src/main/java/com/bluehair/hanghaefinalproject/sse/controller/NotificationController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/sse/controller/NotificationController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +37,7 @@ public class NotificationController {
             @ApiResponse(responseCode = "5000", description = "SSE 연결 실패")
     })
     @Operation(summary = "SSE 연결")
-    @GetMapping(value="/api/subscribe", produces = "text/event-stream")
+    @GetMapping(value="/api/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestHeader(value="Last-Event-ID", required = false, defaultValue = "") String lastEventId ){
         return notificationService.subscribe(lastEventId, userDetails.getMember().getId());
     }
