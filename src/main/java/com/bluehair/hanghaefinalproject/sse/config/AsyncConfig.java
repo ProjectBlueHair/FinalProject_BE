74739d47683c03feed945f345821fed9ce2a6b1e
sooni2 <jinong22@gmail.com>
2023-01-31 14:57:29 +0900

    feat: AsyncConfig 추가

---
+++ src/main/java/com/bluehair/hanghaefinalproject/sse/config/AsyncConfig.java
@@ -0,0 +1,30 @@
+package com.bluehair.hanghaefinalproject.sse.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(30);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("NOTIFICATION-ASYNC");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return super.getAsyncUncaughtExceptionHandler();
+    }
+}
