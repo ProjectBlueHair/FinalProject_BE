4daf566cb24271d28e5c4619f7d1977e5239d767
JeekLee <j3kkun@gmail.com>
2023-02-01 02:09:24 +0900

    feat: Facade Pattern 적용

---
+++ src/main/java/com/bluehair/hanghaefinalproject/post/service/PostServiceFacade.java
@@ -0,0 +1,34 @@
+package com.bluehair.hanghaefinalproject.post.service;
+
+import com.bluehair.hanghaefinalproject.collaboRequest.dto.serviceDto.CollaboRequestDetailsDto;
+import com.bluehair.hanghaefinalproject.collaboRequest.service.CollaboRequestService;
+import com.bluehair.hanghaefinalproject.music.service.MusicService;
+import com.bluehair.hanghaefinalproject.post.dto.serviceDto.PostDto;
+import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostServiceFacade {
+    private final PostService postService;
+    private final CollaboRequestService collaboRequestService;
+    private final MusicService musicService;
+
+    @Transactional
+    public Long createPost(CustomUserDetails userDetails, PostDto postDto, CollaboRequestDetailsDto collaboRequestDetailsDto,
+                           List<String> musicPartList, List<MultipartFile> musicFileList) {
+        Long postId = postService.createPost(postDto, userDetails.getMember().getNickname());
+        Long collaboRequestId = collaboRequestService.collaboRequest(postId, collaboRequestDetailsDto, userDetails.getMember());
+        collaboRequestService.approveCollaboRequest(collaboRequestId, userDetails.getMember());
+        musicService.saveMusic(musicFileList, postId, musicPartList, collaboRequestId);
+
+        return postId;
+    }
+}
