4e10b3b0de4d0ad06bb7915a6837a1af326c0818
yjk <djwhs415@gmail.com>
2023-02-01 00:02:00 +0900

    fix : addEndpoint 수정

--- src/main/java/com/bluehair/hanghaefinalproject/webSocket/config/ChatConfig.java
+++ src/main/java/com/bluehair/hanghaefinalproject/webSocket/config/ChatConfig.java
@@ -1,20 +1,20 @@
 package com.bluehair.hanghaefinalproject.webSocket.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
-@Component
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")
-                .setAllowedOrigins("http://localhost:3000")
-                .setAllowedOrigins("https://oncounter.co.kr")
+        registry.addEndpoint("/ws/chat/info")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .setAllowedOriginPatterns("https://oncounter.co.kr")
                 .withSockJS();
     }
 
