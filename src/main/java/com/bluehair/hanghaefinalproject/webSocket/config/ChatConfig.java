9dd5563836eda0ab68c248955706dc63ee162e7d
yjk <djwhs415@gmail.com>
2023-02-01 00:13:22 +0900

    fix : setAllowedOrigins 설정

--- src/main/java/com/bluehair/hanghaefinalproject/webSocket/config/ChatConfig.java
+++ src/main/java/com/bluehair/hanghaefinalproject/webSocket/config/ChatConfig.java
@@ -13,8 +13,7 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat/info")
-                .setAllowedOriginPatterns("http://localhost:3000")
-                .setAllowedOriginPatterns("https://oncounter.co.kr")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
