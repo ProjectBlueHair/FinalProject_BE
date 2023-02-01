9cfdb40601219ef43111cce4b62fe9f69e47e846
yjk <djwhs415@gmail.com>
2023-02-01 23:22:01 +0900

    fix : 메세지 전송 반환 방식 변경

--- src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
@@ -4,29 +4,22 @@ import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
 import com.bluehair.hanghaefinalproject.webSocket.dto.request.RequestMessageDto;
 import com.bluehair.hanghaefinalproject.webSocket.dto.service.SaveMessageDto;
 import com.bluehair.hanghaefinalproject.webSocket.service.MessageService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @Slf4j
 public class MessageController {
     private final MessageService messageService;
-    
-    @MessageMapping("/chat/message/{roomId}") // app/chat/message/{roomId} 로 메세지를 보냄
+    @PostMapping ("/chat/message/{roomId}") // app/chat/message/{roomId} 로 메세지를 보냄
     @SendTo("/topic/chat/room/{roomId}") // topic/chat/room/{roomId} 를 구독하고 있는 클라이언트에 return 함.
-    public SaveMessageDto enter(@DestinationVariable Long roomId, @RequestBody RequestMessageDto requestMessageDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    public SaveMessageDto enter(@PathVariable Long roomId, @RequestBody RequestMessageDto requestMessageDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
 
         SaveMessageDto saveMessageDto = messageService.saveMessage(roomId,requestMessageDto.toMessageDto(), userDetails.getMember().getNickname());
 
