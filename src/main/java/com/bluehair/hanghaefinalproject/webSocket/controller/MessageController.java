667fd8dc76eef181b72cb4d89565e8ab1a542af2
yjk <djwhs415@gmail.com>
2023-02-01 22:55:34 +0900

    fix : 메세지 전송 반환 방식 변경

--- src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
@@ -1,8 +1,8 @@
 package com.bluehair.hanghaefinalproject.webSocket.controller;
 
-import com.bluehair.hanghaefinalproject.common.response.success.SuccessResponse;
 import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
 import com.bluehair.hanghaefinalproject.webSocket.dto.request.RequestMessageDto;
+import com.bluehair.hanghaefinalproject.webSocket.dto.service.SaveMessageDto;
 import com.bluehair.hanghaefinalproject.webSocket.service.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,34 +10,26 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import static com.bluehair.hanghaefinalproject.common.response.success.SucessCode.MESSAGE_LIST;
 
-@Tag(name = "메세지 전송", description = "메세지 전송 API")
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Slf4j
 public class MessageController {
-
-    private final SimpMessageSendingOperations sendingOperations;
     private final MessageService messageService;
-    @Tag(name = "message")
-    @Operation(summary = "메세지 전송", description = "메세지 전송")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "2000", description = "메세지 전송 성공")
-    })
-    @MessageMapping("/chat/message/{roomId}")
-    @SendTo("/topic/chat/room/{roomId}")
-    public ResponseEntity<SuccessResponse<Object>> enter(@DestinationVariable Long roomId, @RequestBody RequestMessageDto requestMessageDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    
+    @MessageMapping("/chat/message/{roomId}") // app/chat/message/{roomId} 로 메세지를 보냄
+    @SendTo("/topic/chat/room/{roomId}") // topic/chat/room/{roomId} 를 구독하고 있는 클라이언트에 return 함.
+    public SaveMessageDto enter(@DestinationVariable Long roomId, @RequestBody RequestMessageDto requestMessageDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
 
-        return SuccessResponse.toResponseEntity(MESSAGE_LIST,messageService.saveMessage(roomId,requestMessageDto.toMessageDto(), userDetails.getMember().getNickname()));
+        SaveMessageDto saveMessageDto = messageService.saveMessage(roomId,requestMessageDto.toMessageDto(), userDetails.getMember().getNickname());
+
+        return saveMessageDto;
     }
 }
