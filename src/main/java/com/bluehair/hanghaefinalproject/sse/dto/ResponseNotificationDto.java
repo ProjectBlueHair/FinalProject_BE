7b72ef6a93459d8f25d324ca878e5938249723fa
sooni2 <jinong22@gmail.com>
2023-02-01 14:29:32 +0900

    remove: NotificationDto 주석처리
    - 사용하지 않음

--- src/main/java/com/bluehair/hanghaefinalproject/sse/dto/ResponseNotificationDto.java
+++ src/main/java/com/bluehair/hanghaefinalproject/sse/dto/ResponseNotificationDto.java
@@ -1,37 +1,37 @@
-package com.bluehair.hanghaefinalproject.sse.dto;
-
-import com.bluehair.hanghaefinalproject.common.service.LocalDateTimeConverter;
-import com.bluehair.hanghaefinalproject.sse.entity.Notification;
-import com.bluehair.hanghaefinalproject.sse.entity.RedirectionType;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
-
-@Schema(description = "알림 Dto")
-@Getter
-@Setter
-public class ResponseNotificationDto {
-    private Long id;
-    private String content;
-    private RedirectionType type;
-    private Long typeId;
-    private Long postId;
-    private String sender;
-    private String senderImg;
-    private Boolean isRead;
-    @Schema(description = "알림 발송 시간", example = "시간")
-    private String createdAt;
-
-    @Builder
-    public ResponseNotificationDto(Notification notification) {
-        this.id = notification.getId();
-        this.content = notification.getContent();
-        this.type = notification.getUrl().getType();
-        this.typeId = notification.getUrl().getTypeId();
-        this.postId = notification.getUrl().getPostId();
-        this.sender = notification.getSender().getSender();
-        this.senderImg = notification.getSender().getSenderImg();
-        this.isRead = notification.getIsRead();
-        this.createdAt = LocalDateTimeConverter.timeToString(notification.getCreatedAt());
-    }
-
-}
+//package com.bluehair.hanghaefinalproject.sse.dto;
+//
+//import com.bluehair.hanghaefinalproject.common.service.LocalDateTimeConverter;
+//import com.bluehair.hanghaefinalproject.sse.entity.Notification;
+//import com.bluehair.hanghaefinalproject.sse.entity.RedirectionType;
+//import io.swagger.v3.oas.annotations.media.Schema;
+//import lombok.*;
+//
+//@Schema(description = "알림 Dto")
+//@Getter
+//@Setter
+//public class ResponseNotificationDto {
+//    private Long id;
+//    private String content;
+//    private RedirectionType type;
+//    private Long typeId;
+//    private Long postId;
+//    private String sender;
+//    private String senderImg;
+//    private Boolean isRead;
+//    @Schema(description = "알림 발송 시간", example = "시간")
+//    private String createdAt;
+//
+//    @Builder
+//    public ResponseNotificationDto(Notification notification) {
+//        this.id = notification.getId();
+//        this.content = notification.getContent();
+//        this.type = notification.getUrl().getType();
+//        this.typeId = notification.getUrl().getTypeId();
+//        this.postId = notification.getUrl().getPostId();
+//        this.sender = notification.getSender().getSender();
+//        this.senderImg = notification.getSender().getSenderImg();
+//        this.isRead = notification.getIsRead();
+//        this.createdAt = LocalDateTimeConverter.timeToString(notification.getCreatedAt());
+//    }
+//
+//}
