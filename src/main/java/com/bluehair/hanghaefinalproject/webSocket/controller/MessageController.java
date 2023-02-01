aa1849dbc4264af5720db4e9df6067d63aa09a1a
yjk <djwhs415@gmail.com>
2023-02-01 23:40:55 +0900

    fix : 메세지 전송 파라미터 변경

--- src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
+++ src/main/java/com/bluehair/hanghaefinalproject/webSocket/controller/MessageController.java
@@ -1,27 +1,26 @@
 package com.bluehair.hanghaefinalproject.webSocket.controller;
 
-import com.bluehair.hanghaefinalproject.security.CustomUserDetails;
 import com.bluehair.hanghaefinalproject.webSocket.dto.request.RequestMessageDto;
 import com.bluehair.hanghaefinalproject.webSocket.dto.service.SaveMessageDto;
 import com.bluehair.hanghaefinalproject.webSocket.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
 @Slf4j
 public class MessageController {
     private final MessageService messageService;
-    @PostMapping ("/chat/message/{roomId}") // app/chat/message/{roomId} 로 메세지를 보냄
+    @MessageMapping("/chat/message/{roomId}") // app/chat/message/{roomId} 로 메세지를 보냄
     @SendTo("/topic/chat/room/{roomId}") // topic/chat/room/{roomId} 를 구독하고 있는 클라이언트에 return 함.
-    public SaveMessageDto enter(@PathVariable Long roomId, @RequestBody RequestMessageDto requestMessageDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    public SaveMessageDto enter(@DestinationVariable Long roomId, RequestMessageDto requestMessageDto) {
 
-        SaveMessageDto saveMessageDto = messageService.saveMessage(roomId,requestMessageDto.toMessageDto(), userDetails.getMember().getNickname());
+        SaveMessageDto saveMessageDto = messageService.saveMessage(roomId,requestMessageDto.toMessageDto(), requestMessageDto.getNickname());
 
         return saveMessageDto;
     }
