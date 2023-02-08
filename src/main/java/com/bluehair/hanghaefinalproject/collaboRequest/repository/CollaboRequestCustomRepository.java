47bb259e9dc9f35364d931d47f5c94d314c99c57
sooni2 <jinong22@gmail.com>
2023-02-08 13:49:38 +0900

    feat: CollaboRequest QueryDSL 적용

---
+++ src/main/java/com/bluehair/hanghaefinalproject/collaboRequest/repository/CollaboRequestCustomRepository.java
@@ -0,0 +1,26 @@
+package com.bluehair.hanghaefinalproject.collaboRequest.repository;
+
+import com.bluehair.hanghaefinalproject.collaboRequest.entity.CollaboRequest;
+import com.bluehair.hanghaefinalproject.post.entity.Post;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface CollaboRequestCustomRepository {
+    List<CollaboRequest> findAllByPostId(Long id);
+
+    List<CollaboRequest> findAllByPost(Post post);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE from CollaboRequest c where c.post = :Post")
+    void deleteAllByPost(@Param("Post") Post post);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE CollaboRequest c set c.nickname = :after where c.nickname = :before")
+    void updateNickname(@Param("before") String before, @Param("after") String after);
+}
