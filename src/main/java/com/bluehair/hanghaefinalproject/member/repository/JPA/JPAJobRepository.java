775deba11547a00be667a1beddb7c1ac20449337
JeekLee <j3kkun@gmail.com>
2023-02-08 17:59:09 +0900

    fix: SaveJobList -> SaveAll 변경

--- src/main/java/com/bluehair/hanghaefinalproject/member/repository/JPA/JPAJobRepository.java
+++ src/main/java/com/bluehair/hanghaefinalproject/member/repository/JPA/JPAJobRepository.java
@@ -3,9 +3,6 @@ package com.bluehair.hanghaefinalproject.member.repository.JPA;
 import com.bluehair.hanghaefinalproject.member.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface JPAJobRepository extends JpaRepository<Job, Long> {
     Job save(Job job);
-    void saveJobList(List<Job> jobList);
 }
